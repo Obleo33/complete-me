93504274321d09704ac8fbea0a8718592f40954e
Franklin Crosby <obleo33@gmail.com>
2017-02-14 14:26:22 -0700

    first commit

---
+++ test/node-test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import Trie from '../scripts/trie';
+import Node from '../scripts/node'
+
+describe('testing Node functionality',()=>{
+
+  let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
+  });
+
+  it('',()=>{
+  })
+});
---
+++ test/trie-test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import Trie from '../scripts/trie';
+import Node from '../scripts/node'
+
+describe('testing Trie functionality',()=>{
+
+  let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
+  });
+
+  it('',()=>{
+  })
+});
